acc02cd9b717ab254dd61a17a1ef741d9a06d065
longjunjieaaa <95904787+longjunjieaaa@users.noreply.github.com>
2022-02-10 15:18:53 +0800

    prac3

--- broken_score.py
+++ broken_score.py
@@ -1,23 +1,13 @@
-score = float(input("Enter score: "))
-if score < 0:
-    print("Invalid score")
-else:
-    if score > 100:
+ def main():
+    score = float(input("Enter score: "))
+    print(search_result(score))
+def search_result(score):
+    if score < 0 or score > 100:
         print("Invalid score")
-    if score > 50:
+    elif score >= 90:
+        print("Excellent")
+    elif score >= 50:
         print("Passable")
-    if score > 90:
-    print("Excellent")
-if score < 50:
-    print("Bad")
-
-
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else  :
-    print("Bad")
+    else:
+        print("Bad")
+main()
