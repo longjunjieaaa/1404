e8839719472202e2f354a085ffd10c29141cc87f
longjunjieaaa <95904787+longjunjieaaa@users.noreply.github.com>
2022-02-10 16:01:59 +0800

    Add files via upload

---
+++ prac5/word_occurrences.py
@@ -0,0 +1,31 @@
+Text: this is a collection of words of nice words this is a fun thing it is
+a : 2
+collection : 1
+fun : 1
+is : 3
+it : 1
+nice : 1
+of : 2
+thing : 1
+this : 2
+words : 2
+
+print("{:{}} = {}".format(x, y, z))
+
+a :          2
+collection : 1
+fun :        1
+
+word_count = {}
+text = input("Text: ")
+words = text.split()
+for word in words:
+    frequency = word_count.get(word, 0)
+    word_count[word] = frequency + 1
+
+words = list(word_to_count.keys())
+words.sort()
+
+max_length = max((len(word) for word in words))
+for word in words:
+    print("{:{}} : {}".format(word, max_length, word_count[word]))
